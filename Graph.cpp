4a279ef97a8893086d1846efcfc1351eaa0b3597
mihonglee <74884482+mihonglee@users.noreply.github.com>
2020-11-30 11:28:11 -0500

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -46,6 +46,9 @@ void Graph::removeVertex(std::string label) {
     }
 };
 
+/*Adds an edge of value weight to the graph between the vertex with label1 and the vertex with
+label2. A vertex with label1 and a vertex with label2 must both exist, there must not already be
+an edge between those vertices, and a vertex cannot have an edge to itself.*/
 
 void Graph::addEdge(std::string label1, std::string label2, unsigned long weight) {
   bool checkA = false, checkB = false;
@@ -64,6 +67,9 @@ void Graph::addEdge(std::string label1, std::string label2, unsigned long weight
     }
 };
 
+/*Removes the edge from the graph between the vertex with label1 and the vertex with label2. A
+vertex with label1 and a vertex with label2 must both exist and there must be an edge between
+those vertices*/
 
 void Graph::removeEdge(std::string label1, std::string label2) {
    Edge* edge = new Edge();
@@ -76,6 +82,10 @@ void Graph::removeEdge(std::string label1, std::string label2) {
    }
 };
 
+/*Calculates the shortest path between the vertex with startLabel and the vertex with endLabel
+using Dijkstra's Algorithm. A vector is passed into the method that stores the shortest path
+between the vertices. The return value is the sum of the edges between the start and end
+vertices on the shortest path. */
 
 unsigned long Graph::shortestPath(std::string startLabel, std::string endLabel, std::vector<std::string> &path) {
    
