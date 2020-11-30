978a86b7944bc8e8330a56e4d3bf0205a54e9564
AbrorKh <43052046+AbrorKh@users.noreply.github.com>
2020-11-29 20:24:05 -0500

    Update Edge.hpp
    extra global variable?

--- Edge.hpp
+++ Edge.hpp
@@ -1,13 +1,12 @@
 #ifndef EDGE_H
 #define EDGE_H
 #include <string>
-const std::string EMPTY_STR = " ";
 
 class Edge {
 public:
     Edge() {
-        u = EMPTY_STR;
-        v = EMPTY_STR;
+        u = " ";
+        v = " ";
         edgeWeight = 0;
     };
     ~Edge();
