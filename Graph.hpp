0297f3619d6798b8a50c783491f8a6b61b3c96a4
AbrorKh <43052046+AbrorKh@users.noreply.github.com>
2020-11-29 21:25:44 -0500

    Update Graph.hpp
    no need for print method

--- Graph.hpp
+++ Graph.hpp
@@ -20,7 +20,6 @@ public:
     void addEdge(std::string label1, std::string label2, unsigned long weight);
     void removeEdge(std::string label1, std::string label2);
     unsigned long shortestPath(std::string startLabel, std::string endLabel, std::vector<std::string> &path);
-    void printShortestPath(std::map<std::string, std::string> startLabel, std::string endLabel, std::vector<std::string> &path);
 
 private:
     friend class Edge;
